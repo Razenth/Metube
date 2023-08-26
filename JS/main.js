bc5390b663052aea21ab8c0b1a4dba1dff6d9d9d
Santi <santiago.gonzalezagudelo@gmail.com>
2023-08-26 12:43:54 -0500

    feat:art: : HTML y CSS de Barra Lateral hecho

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+let menuIcon = document.querySelector('.menu-icon');
+let sidebar = document.querySelector('.sidebar');
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
